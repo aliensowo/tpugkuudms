b1d41b8c232151bda1a08b8c858e8b2b29371bd3
Kalinkin <kalinkin@cyberborea.tech>
2023-05-19 06:39:16 +0300

    task 2 to guide list

--- logic/I_logic_page.py
+++ logic/I_logic_page.py
@@ -4,7 +4,8 @@ from PyQt5.QtWidgets import QTableWidgetItem
 
 from gui.I_guide_page import Ui_MainWindow as IPage
 from models.database import SessionLocal
-from models.crud import criteria, contractors, contractors_to_contractors, criteria_to_criteria
+from models.crud import criteria, contractors, contractors_to_contractors, criteria_to_criteria, neighborhood_directory
+from models.crud import objects_contracts
 from logic._base_class_logic import BaseClassLogic
 from logic.D_logic_page import DLogicPage
 from logic.E_logic_page import ELogicPage
@@ -20,9 +21,12 @@ class ILogicPage(QtWidgets.QMainWindow, BaseClassLogic):
         self.winner_window = None
         self.ui = IPage()
         self.ui.setupUi(self)
-        self.ui.menu.triggered.connect(self.display_criteria_list)
+        self.ui.action_5.triggered.connect(self.display_criteria_list)
+        self.ui.action_3.triggered.connect(self.display_directions)
+        self.ui.action_4.triggered.connect(self.display_contracts)
 
     def display_criteria_list(self):
+        self.ui.tableWidget.clear()
         # Criteria set
         criteria_data_list = self.get_data_criteria()
         if criteria_data_list:
@@ -39,3 +43,33 @@ class ILogicPage(QtWidgets.QMainWindow, BaseClassLogic):
                 self.ui.tableWidget.setItem(criteria_data_list.index(row), 0, id_item)
                 self.ui.tableWidget.setItem(criteria_data_list.index(row), 1, name_item)
 
+    def display_directions(self):
+        self.ui.tableWidget.clear()
+        with SessionLocal() as session:
+            d_list = neighborhood_directory.list(session)
+            if d_list:
+                self.ui.tableWidget.setColumnCount(d_list[0].__len__())
+                self.ui.tableWidget.setRowCount(d_list.__len__())
+                self.ui.tableWidget.setHorizontalHeaderLabels(list(d_list[0].keys()))
+                for row in d_list:
+                    for key in row.keys():
+                        item = QTableWidgetItem()
+                        item.setFlags(item.flags() ^ QtCore.Qt.ItemFlag.ItemIsEditable)
+                        item.setText(str(row[key]))
+                        self.ui.tableWidget.setItem(d_list.index(row), list(row.keys()).index(key), item)
+
+
+    def display_contracts(self):
+        self.ui.tableWidget.clear()
+        with SessionLocal() as session:
+            d_list = objects_contracts.list(session)
+            if d_list:
+                self.ui.tableWidget.setColumnCount(d_list[0].__len__())
+                self.ui.tableWidget.setRowCount(d_list.__len__())
+                self.ui.tableWidget.setHorizontalHeaderLabels(list(d_list[0].keys()))
+                for row in d_list:
+                    for key in row.keys():
+                        item = QTableWidgetItem()
+                        item.setFlags(item.flags() ^ QtCore.Qt.ItemFlag.ItemIsEditable)
+                        item.setText(str(row[key]))
+                        self.ui.tableWidget.setItem(d_list.index(row), list(row.keys()).index(key), item)
